5db1187bba4493315d5aa2dfb036ec22ff9b1aa7
ronaaal <ronalalvaradooo@hotmail.com>
2022-10-25 00:41:26 -0500

    First commit

---
+++ src/Contenido.java
@@ -0,0 +1,49 @@
+import java.sql.SQLOutput;
+
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("[ Conceptos básicos de programación ]");
+        System.out.print("1. Algoritmo: ");
+        System.out.println("Un conjunto de pasos para una determinada tarea.");
+        System.out.print("2. Lenguaje: ");
+        System.out.println("lenguaje de computadora que los programadores utilizan para comunicarse y para desarrollar programas.");
+        System.out.print("3. Código: ");
+        System.out.println("Conjunto de palabras o símbolos que contienen instrucciones para la computadora.");
+        System.out.print("4. Código Binario: ");
+        System.out.println("Código formado solo por 0 y 1 que tiene como función codificar algo.");
+        System.out.print("5. Backup: ");
+        System.out.println("Copia de archivos en otro dispositivo para guardar información.");
+        System.out.print("6. Big Data: ");
+        System.out.println("Almacenamiento de grandes cantidades de datos.");
+        System.out.print("7. BIOS: ");
+        System.out.println("Sistema utilizado para listar los dispositivos estándar de entrada (input) y salida (output) de una computadora");
+        System.out.print("8. Bit: ");
+        System.out.println("Abreviatura de binary digit, es decir, representa solo un valor binario (cero o uno).");
+        System.out.print("9. Clase: ");
+        System.out.println("Proyecto de toda objeto.");
+        System.out.print("10. Compilador: ");
+        System.out.println("Transforma el código en un lenguaje de programación a un lenguaje de máquina");
+        System.out.println(" ");
+        System.out.println("[ Conceptos de Fundamentos Computacionales ]");
+        System.out.print("11. Datos: ");
+        System.out.println("Es una representación simbólica de un atributo o variable cuantitativa o cualitativa.");
+        System.out.print("12. Información: ");
+        System.out.println("Conjunto organizado de datos procesados");
+        System.out.print("13. Conocimiento: ");
+        System.out.println("Habilidades esenciales que se necesitan para usar las computadoras y tecnologías similares, relacionadas de manera eficiente.");
+        System.out.print("14. Datos numéricos: ");
+        System.out.println(" El tipo numérico es el conjunto de los valores numéricos. Estos pueden representarse en dos formas distintas.");
+        System.out.print("15. Datos lógicos: ");
+        System.out.println("Es aquel dato que sólo puede tomar uno de dos valores: cierto o verdadero (true) y falso (false).");
+        System.out.print("16. Datos tipo carácter: ");
+        System.out.println("Es el conjunto finito y ordenado de caracteres que la computadora reconoce.");
+        System.out.print("17. Constantes: ");
+        System.out.println("Una constante es un valor que no puede ser alterado/modificado durante la ejecución de un programa, únicamente puede ser leído.");
+        System.out.print("18. Variables: ");
+        System.out.println("Es donde se almacenan y se recuperan los datos de un programa.");
+        System.out.print("19. Expresiones: ");
+        System.out.println("Son combinaciones de constantes, variables y operadores para ejecutar una operación.");
+        System.out.print("20. Instrucciones: ");
+        System.out.println("Son las sentencias o podríamos decir pasos, que se ejecutan en un programa.");
+    }
+}
